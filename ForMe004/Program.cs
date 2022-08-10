0dbdbcd821670dab47547e8aabd4ebe867829c42
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-10 16:04:19 +0300

    Add ForMe004 Last Number Of Three Digit Number

---
+++ ForMe004/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число и я покажу вам его последнее. УУУУ, магия!!!");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+int lastNumber = threeDigitNumber % 10;
+Console.WriteLine(lastNumber);
