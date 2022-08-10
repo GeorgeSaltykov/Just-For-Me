1976a6a6f605c9907642cbc522414802a53c829d
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-10 15:02:06 +0300

    Add ForMe001

--- ForMe001/Program.cs
+++ ForMe001/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Программа для проверки является ли первое число квадратом второго. Введите число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine($"Число {firstNumber} является квадратом {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"Число {firstNumber} не является квадратом {secondNumber}");
+}
