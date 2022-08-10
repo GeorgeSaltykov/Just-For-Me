55cf60f7e11840481a63765cff18edfd38530f0c
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-10 15:53:33 +0300

    Add ForMe003 range from N to -N

---
+++ ForMe003/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Программа выведет для вас все целые числа от -n до n, где n это вами введенное число. Так что вводите число, не стесняйтесь!");
+int number = Convert.ToInt32(Console.ReadLine());
+int minusNumber = - number;
+while(minusNumber <= number)
+{
+    Console.WriteLine(minusNumber);
+    minusNumber++;
+}
